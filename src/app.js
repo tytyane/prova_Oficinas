de49396e493d39f8f7ffe93c85f33904b57ad30e
tytyane <nefertitidk05@gmail.com>
2024-10-14 17:17:24 -0300

    :)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express"
+import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import workshop_route from "./routes/workshop_route.js"
 import vehicle_route from "./routes/vehicle_route.js"
 import maintenance_route from "./routes/maintenance_route.js"
 
+dotenv.config()
 connectDB()
 
-const port = 3000
+const PORT = process.env.API_PORT || 3000
 const app = express()
 app.use(express.json())
 
@@ -14,6 +16,6 @@ app.use('/oficina', workshop_route)
 app.use('/veiculo', vehicle_route)
 app.use('/manutencao', maintenance_route)
 
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+app.listen(process.env.API_PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
 
 export default app
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/oficinas')
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`)
         console.log('MongoDB conectado om sucesso!')
     } catch(error){
         console.erro('Erro ao conectar ao MongoDB:', error)
