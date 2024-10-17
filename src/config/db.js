92d9f61d65c174fad921a95106a4631e79343245
tytyane <nefertitidk05@gmail.com>
2024-10-17 15:15:39 -0300

    :)

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.URI)
+        await mongoose.connect(`${process.env.URI}`)
         console.log('MongoDB conectado om sucesso!')
     } catch(error){
         console.error('Erro ao conectar ao MongoDB:', error)
